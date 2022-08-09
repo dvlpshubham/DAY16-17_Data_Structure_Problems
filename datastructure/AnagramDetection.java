f608beeffd7c8c1c2d475a3bc6f04e1eec5f83bb
Shubham Raj <shubhamaryan805@gmail.com>
2022-08-09 14:17:43 +0530

    Checking The Elements Of Two Arrays Are Anagram Or Not

---
+++ datastructure/AnagramDetection.java
@@ -0,0 +1,57 @@
+/*@Purpose::To find two strings are anagram or not
+    a. Desc -> One string is an anagram of another if the second is simply a
+       rearrangement of the first. For example, 'heart' and 'earth' are anagrams...
+    b. I/P -> Take 2 Strings as Input such abcd and dcba and Check for Anagrams
+    c. Logic -> Take two strings convert them to char array ,sort them compare character by character if any character mismatches return false
+    d. O/P -> return true if anagram else return false
+  @Date::08-04-2022
+  @file::AnagramDetection.java
+  @author::Shubham Raj
+ */
+
+package datastructure;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AnagramDetection {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Enter the String 1");
+            String string1 = scanner.next();
+            System.out.println("Enter the String 2");
+            String string2 = scanner.next();
+            AnagramDetection anagramDetection = new AnagramDetection();
+            if (anagramDetection.anagram(string1, string2)) {
+                System.out.println("Strings are anagram");
+            } else {
+                System.out.println("Strings are not anagram");
+            }
+        }
+    }
+
+    /*
+     * @purpose: Ability to check two strings are of same characters
+     *
+     * @param: Two different strings of same length
+     *
+     * @function : converts string to char array sort them and compare character by
+     * character if equal return true
+     *
+     * @return: Returns true on success and false on failure
+     */
+    private boolean anagram(String string1, String string2) {
+        if (string1.length() != string2.length())
+            return false;
+        char[] array1 = string1.toCharArray();
+        Arrays.sort(array1);
+        char[] array2 = string2.toCharArray();
+        Arrays.sort(array2);
+        for (int i = 0; i < array1.length; i++) {
+            if (array1[i] != array2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
